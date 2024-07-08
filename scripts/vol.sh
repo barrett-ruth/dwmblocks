49e7c8c09bfce1f5e8487184c42c70bacc2a4ee5
Barrett Ruth <br.barrettruth@gmail.com>
2024-07-08 18:25:29 -0500

    fix: get sink id correctly

--- scripts/vol.sh
+++ scripts/vol.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 sink_info="$(pacmd list-sinks | sed '/\* index/ p')"
-sink_id="$(echo "$sink_info" | rg 'index' | tail -c 2)"
+sink_id="$(echo "$sink_info" | rg 'index')"
+sink_id="${sink_id##* }"
 
 vol=$(pactl get-sink-volume "$sink_id" | awk '{ print $5 }')
 vol="${vol%?}"
