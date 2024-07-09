a424183216903c3e49446a47ac98d8078562dd5c
Barrett Ruth <br.barrettruth@gmail.com>
2024-07-09 11:43:23 -0500

    feat(wifi): fix no wifi found

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-station="$(iwctl station list | rg 'connected' | awk '{ print $2 }')"
-station_info="$(iwctl station "$station" show)"
-ssid="$(echo "${station_info#*Connected network}" | head -n 1 | xargs)"
-if [ -z "$ssid" ]; then
-  if ping archlinux.org -c 1 >/dev/null 2>&1; then
-    ssid='eth'
-  fi
+station="$(iwctl station list | rg ' connected' | awk '{ print $2 }')"
+if [ -n "$station" ]; then
+  station_info="$(iwctl station "$station" show)"
+  ssid="$(echo "${station_info#*Connected network}" | head -n 1 | xargs)"
 fi
 ssid="${ssid:-n/a}"
 
