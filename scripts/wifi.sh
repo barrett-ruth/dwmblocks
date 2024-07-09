e5c35a2ad71ded9a031ddc973322257fe2bb73cf
Barrett Ruth <br.barrettruth@gmail.com>
2024-07-09 10:46:10 -0500

    feat(wifi): extract entire wifi script

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-ssid="$(iwctl station wlan0 get-networks | grep '>' | awk '{ print $4 }')"
+station="$(iwctl station list | rg 'connected' | awk '{ print $2 }')"
+station_info="$(iwctl station "$station" show)"
+ssid="$(echo "${station_info#*Connected network}" | head -n 1 | xargs)"
 if [ -z "$ssid" ]; then
   if ping archlinux.org -c 1 >/dev/null 2>&1; then
     ssid='eth'
