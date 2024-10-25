cda1ee966430863f64631e3c91e1a9cf0ffda579
Barrett Ruth <br.barrettruth@gmail.com>
2024-10-25 10:13:41 -0400

    feat: battery notifications

--- scripts/bat.sh
+++ scripts/bat.sh
@@ -4,6 +4,9 @@ test -f /sys/class/power_supply/BAT0/capacity || exit 0
 
 bat="$(cat /sys/class/power_supply/BAT0/capacity)"
 
+[ "$bat" -eq 30 ] && notify-send --urgency normal "30%"
+[ "$bat" -eq 15 ] && notify-send --urgency critical "15%"
+
 [ -n "$bat" ] && echo "$bat% │ "
 
 unset bat
